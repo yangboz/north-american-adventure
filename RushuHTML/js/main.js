071c41c308324bcc55c6285f3d7e77b8d9e37369
yangboz <YoungWelle@gmail.com>
2014-10-22 14:06:12 +0800

    FIXED:invalid css/js/assets file path

--- RushuHTML/js/main.js
+++ RushuHTML/js/main.js
@@ -22,9 +22,9 @@ jQuery(document).ready(function($) {
 
     //Fullscreen Background Image Slideshow
     $.backstretch([
-        "../../assets/img/bg2/01_home.jpg", 
-        "../../assets/img/bg2/02_about.jpg",
-        "../../assets/img//bg2/03_facts.jpg"
+        "assets/img/bg2/01_home.jpg",
+        "assets/img/bg2/02_about.jpg",
+        "assets/img/bg2/03_facts.jpg"
     ], {duration: 3000, fade: 750});
 
 
