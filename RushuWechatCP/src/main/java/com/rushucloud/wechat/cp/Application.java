e4c4585b18b78a3735a0d4eeefa6fa65874d3086
yangboz <YoungWelle@gmail.com>
2015-01-15 14:20:06 +0800

    CHANGED:applied the getDepartmentUsers function test for WeChatCP

--- RushuWechatCP/src/main/java/com/rushucloud/wechat/cp/Application.java
+++ RushuWechatCP/src/main/java/com/rushucloud/wechat/cp/Application.java
@@ -1,9 +1,12 @@
 package com.rushucloud.wechat.cp;
 
+import java.util.List;
+
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.cp.api.WxCpInMemoryConfigStorage;
 import me.chanjar.weixin.cp.api.WxCpServiceImpl;
 import me.chanjar.weixin.cp.bean.WxCpMessage;
+import me.chanjar.weixin.cp.bean.WxCpUser;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +19,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 	private static Logger LOG = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws WxErrorException {
 		SpringApplication.run(Application.class, args);
 		// Wechat-CP testing code here:
 		// @see:
@@ -31,18 +34,23 @@ public class Application {
 
 		WxCpServiceImpl wxCpService = new WxCpServiceImpl();
 		wxCpService.setWxCpConfigStorage(config);
-
-		String userId = "yangbozhou";
-		WxCpMessage message = WxCpMessage.TEXT().agentId("2")
-				.toParty("1")
-				.toUser(userId)
-				.content("Foo bar!").build();
-		try {
-			LOG.info("wxMessageSend:"+message.toJson().toString());
-			wxCpService.messageSend(message);
-		} catch (WxErrorException e) {
-			LOG.error(e.toString());
+		LOG.info("wxDepartGet:"+wxCpService.departGet().toString());
+		List<WxCpUser> users = wxCpService.departGetUsers(1, true, 1);
+		for(int i=0;i<users.size();i++)
+		{
+			LOG.info("wxDepartGetUser["+i+":"+users.get(i).toJson().toString());
 		}
+//		String userId = "yangbozhou";
+//		WxCpMessage message = WxCpMessage.TEXT().agentId("2")
+//				.toParty("1")
+//				.toUser(userId)
+//				.content("Foo bar!").build();
+//		try {
+//			LOG.info("wxMessageSend:"+message.toJson().toString());
+//			wxCpService.messageSend(message);
+//		} catch (WxErrorException e) {
+//			LOG.error(e.toString());
+//		}
 	}
 
 }
